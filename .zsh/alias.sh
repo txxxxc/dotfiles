c52ff03aa8ecc56c82d5b46ca0918e7204235d07
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-12-23 18:25:33 +0900

    [add] zsh config

---
+++ .zsh/alias.sh
@@ -0,0 +1,19 @@
+
+alias ls="ls -a -G -F"
+alias record-demo='xcrun simctl io booted recordVideo'
+alias relog='exec $SHELL -l'
+alias ga='git add'
+alias ..='cd ..'
+alias gc='git commit'
+alias gp='git push'
+alias vim='nvim'
+alias vimcf='vim ~/.config/nvim'
+alias dc='docker-compose'
+alias y='yarn'
+alias dot='cd ~/dotfiles'
+alias tm='tmux'
+alias work='cd ~/workspace'
+alias brewic="brew info --cask"
+
+# passful
+alias passful-user="cd ~/workspace/passful/typescript/apps/user"
