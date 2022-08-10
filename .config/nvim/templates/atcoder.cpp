2a783dcf374cffb3655501cf43c19e78714fde55
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-08-10 14:13:13 +0900

    [update] atcoder tmplates

--- .config/nvim/templates/atcoder.cpp
+++ .config/nvim/templates/atcoder.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
 
 int main() {
 
