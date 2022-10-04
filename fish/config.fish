ef037dc4ee759c4550697ed9bd8a4cf6a4b2e15d
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-04 21:04:26 +0900

    [add] asdf settings for fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+## ここ後々変える必要ありそう
+set -x ASDF_DIR '/opt/homebrew/Cellar/asdf/0.10.2/libexec'
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
