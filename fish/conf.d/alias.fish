8fdc627c14f8151b4020e4662c1103209427ea61
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-05 16:28:15 +0900

    [add] alias.fish

---
+++ fish/conf.d/alias.fish
@@ -0,0 +1,41 @@
+alias record-demo='xcrun simctl io booted recordVideo'
+alias relog='exec $SHELL -l'
+alias .='cd ~/dotfiles'
+alias ..='cd ..'
+# git
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gs='git status'
+alias gst='git stash'
+alias gsmain='git switch "$(git symbolic-ref refs/remotes/origin/HEAD | sed s@^refs/remotes/origin/@@)"'
+alias gull='git pull origin "$(git symbolic-ref --short HEAD)"'
+alias gpo='git push origin "$(git symbolic-ref --short HEAD)"'
+alias gph='git push origin "$(git symbolic-ref --short HEAD)"'
+alias gush='git push origin "$(git symbolic-ref --short HEAD)"'
+alias gsc='git switch -c'
+alias gsw='git switch'
+alias vim='nvim'
+alias vimcf='vim ~/.config/nvim'
+alias dc='docker compose'
+alias y='yarn'
+alias dot='cd ~/dotfiles'
+alias tm='tmux'
+alias work='cd ~/workspace'
+alias brewic="brew info --cask"
+alias job="cd ~/projects/job"
+alias me="cd ~/projects/me"
+alias c="powered_cd"
+alias cl="clear"
+alias rmf="rm -rf"
+alias r="ruby"
+alias heorku="heroku"
+alias cat="bat"
+alias ls='lsd -A --group-dirs=last'
+alias ll='lsd -Ahl --total-size --group-dirs=last'
+alias tree='lsd -A --tree --group-dirs=last'
+alias lr='lsd -Ahl --total-size --tree --group-dirs=last'
+alias du='dust'
+alias df='duf'
+alias grep='rg -S'
+
