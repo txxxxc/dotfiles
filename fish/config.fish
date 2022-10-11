da8b4e3756f2ed7ff8591b75643af65ccc50a49f
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-11 22:52:32 +0900

    [add] homebrew configuration to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
-## エラー出るけどasdf自体は動いているので一旦エラーが出力されないように握りつぶします
-source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
-
+  # Returns 0 if fish is interactive - that is, connected to a keyboard.
+if status is-interactive
+	## エラー出るけどasdf自体は動いているので一旦エラーが出力されないように握りつぶします
+	source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
+	eval (/opt/homebrew/bin/brew shellenv)
+end
