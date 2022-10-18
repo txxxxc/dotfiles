3f2b974e601263eb541c6b7ff1a9176a5f66e92e
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-19 00:16:11 +0900

    [update] config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-  # Returns 0 if fish is interactive - that is, connected to a keyboard.
+# Returns 0 if fish is interactive - that is, connected to a keyboard.
 if status is-interactive
 	## エラー出るけどasdf自体は動いているので一旦エラーが出力されないように握りつぶします
-	source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
 	eval (/opt/homebrew/bin/brew shellenv)
+	source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
 end
