ad535089021f4facea5aa5dbfe6c84fcce1eda0d
txxxxc <tomoya113.code@gmail.com>
2022-11-25 22:39:59 +0900

    [update] atcoder.fish

--- fish/functions/atcoder.fish
+++ fish/functions/atcoder.fish
@@ -1,6 +1,6 @@
 function atcoder -d "initialize files for a atcoder problem"
   # if there has already been a stdin.txt, delete it and make a new file
-	mkdir $1 && touch $_/stdin.txt && cd $1
+	mkdir $argv[1] && touch $argv[1]/stdin.txt && cd $argv[1]
   nvim ./index.cpp -o ./stdin.txt -c "11 wincmd +" 
 end
 
