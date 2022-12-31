dc8474d96928f1009d8ccb0d7131db79b7d26bc4
txxxxc <tomoya113.code@gmail.com>
2022-12-31 18:38:17 +0900

    [add] tab setting

--- .config/nvim/_init.lua
+++ .config/nvim/_init.lua
@@ -503,10 +503,14 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 require'nvim-tmux-navigation'.setup {
   disable_when_zoomed = true, -- defaults to false
   keybindings = {
-    left = "<C-h>",
-    down = "<C-j>",
-    up = "<C-k>",
-    right = "<C-l>",
+    left = "<c-h>",
+    down = "<c-j>",
+    up = "<c-k>",
+    right = "<c-l>",
   }
 }
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
