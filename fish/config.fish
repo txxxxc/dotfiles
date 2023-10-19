b1d59c4d70b38a2be16444f0ccdadb313bb467ac
txxxxc <tomoya113.code@gmail.com>
2023-10-20 02:14:47 +0900

    update: fish files

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,27 @@ if status is-interactive
 	eval (/opt/homebrew/bin/brew shellenv)
 	source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
 end
+set -gx XDG_CONFIG_HOME ~/.config
+fish_add_path $HOME/bin
+fish_add_path /opt/homebrew/bin
+
+# golang
+set -x GOPATH (go env GOPATH)
+set -x GOROOT (go env GOROOT)
+set -x GOBIN (go env GOPATH)/bin
+fish_add_path $GOPATH/bin
+fish_add_path $GOROOT/bin
+fish_add_path $GOBIN
+
+# mint
+set -gx MINT_PATH $HOME/.mint
+set -gx MINT_LINK_PATH $MINT_PATH/bin
+fish_add_path MINT_PATH
+
+
+# pnpm
+set -gx PNPM_HOME "/Users/tomoya_tanaka/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
--- fish/functions/atcoder.fish
+++ fish/functions/atcoder.fish
@@ -1,6 +1,6 @@
 function atcoder -d "initialize files for a atcoder problem"
   # if there has already been a stdin.txt, delete it and make a new file
 	mkdir $argv[1] && touch $argv[1]/stdin.txt && cd $argv[1]
-  nvim ./index.cpp -o ./stdin.txt -c "11 wincmd +" 
+  svim ./index.cpp -o ./stdin.txt -c "11 wincmd +" 
 end
 
