bcc440cc6f18d2a8ee93d0fd1dbb1a8753f9eb0a
txxxxc <tomoya113.code@gmail.com>
2023-10-20 02:13:31 +0900

    add: setup-scripts

---
+++ setup-scripts/setup-language-envs.sh
@@ -0,0 +1,9 @@
+anyenv install rbenv
+anyenv install pyenv
+anyenv install nodenv
+anyenv install goenv
+anyenv install jenv
+
+# https://qiita.com/kanican0813/items/c7bb016e353461c8a0df
+# JAVA_HOMEを自動で設定するようにする
+jenv enable-plugin export
