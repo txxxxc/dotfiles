6ddf0037663333fa40e158464b23adf39ee3ec34
txxxxc <tomoya113.code@gmail.com>
2023-11-08 00:39:05 +0900

    update: config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ if status is-interactive
 	source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
 end
 set -gx XDG_CONFIG_HOME ~/.config
+set -gx EDITOR nvim
 fish_add_path $HOME/bin
 fish_add_path /opt/homebrew/bin
 
@@ -21,10 +22,12 @@ set -gx MINT_PATH $HOME/.mint
 set -gx MINT_LINK_PATH $MINT_PATH/bin
 fish_add_path MINT_PATH
 
-
 # pnpm
 set -gx PNPM_HOME "/Users/tomoya_tanaka/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+bind -M insert \cg fgh
+
