01831ddb7874d664722bf509c385925259df1cb8
txxxxc <tomoya113.code@gmail.com>
2023-11-08 00:30:10 +0900

    update: templates/atcoder.cpp

--- .config/sub-nvim/templates/atcoder.cpp
+++ .config/sub-nvim/templates/atcoder.cpp
@@ -3,7 +3,10 @@ using namespace std;
 #define debug(arg) std::cerr << #arg << ": " << (arg) << '\n'
 #define INF ((1LL<<62)-(1LL<<31)) 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
 using ll = long long;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
 
 int main() {
 
