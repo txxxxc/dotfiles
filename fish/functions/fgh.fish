6fb7a21340e63f95b82e797da33cba25ccf8fe52
txxxxc <tomoya113.code@gmail.com>
2023-11-08 00:38:18 +0900

    update: fgh.fish

--- fish/functions/fgh.fish
+++ fish/functions/fgh.fish
@@ -1,5 +1,8 @@
 function fgh -d 'cd to directory under projects using fzf'
-  set REPO_NAME "$(ghq list | fzf-tmux --reverse +m)"
+  set REPO_NAME "$(ghq list | fzf-tmux --reverse +m)"; or begin
+  	return 1
+	end
+
 	if test -d "$(ghq root)/$REPO_NAME"
 		echo "found"
 		cd "$(ghq root)/$REPO_NAME"
