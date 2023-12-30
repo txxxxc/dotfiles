57d3d2e60528b9442c3e71df666227c9ffb62926
txxxxc <tomoya113.code@gmail.com>
2023-12-31 00:33:35 +0900

    update: plugins/nvim-tree-sitter

--- .config/sub-nvim/lua/plugins/nvim-treesitter.lua
+++ .config/sub-nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  run = function()
+  build = function()
     pcall(require('nvim-treesitter.install').update { with_sync = true })
   end,
   config = function()
