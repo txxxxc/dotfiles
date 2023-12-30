b6efacf0b4f46f14349825f93ed4ece192738c59
txxxxc <tomoya113.code@gmail.com>
2023-12-31 00:32:44 +0900

    update: plugins/fern.lua

--- .config/sub-nvim/lua/plugins/fern.lua
+++ .config/sub-nvim/lua/plugins/fern.lua
@@ -1,8 +1,13 @@
 return {
   'lambdalisue/fern.vim',
+  dependencies = {
+    'lambdalisue/nerdfont.vim',
+    'lambdalisue/glyph-palette.vim',
+  },
   config = function()
     -- fern
     vim.g['fern#default_hidden'] = true
+    vim.g['fern#renderer'] = "nerdfont"
     vim.g['fern#default_exclude'] = '^\\%(\\.git\\|\\.DS_Store\\)$'
   end
 }
