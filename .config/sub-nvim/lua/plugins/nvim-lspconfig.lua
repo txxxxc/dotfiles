6697b1af8622dffb7929b7d0adc609b587647631
txxxxc <tomoya113.code@gmail.com>
2023-12-31 00:29:15 +0900

    remove: unncecessary

--- .config/sub-nvim/lua/plugins/nvim-lspconfig.lua
+++
@@ -1,8 +0,0 @@
--- Additional lua configuration, makes nvim stuff amazing
-return {
-  'folke/neodev.nvim',
-  config = function()
-    -- Setup neovim lua configuration
-    require('neodev').setup()
-  end
-}
