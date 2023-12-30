e32d2df974a52e529bf5f58eaebe81cb4ce7a014
txxxxc <tomoya113.code@gmail.com>
2023-12-31 00:31:50 +0900

    update: plugins/indent_blackline.lua

--- .config/sub-nvim/lua/plugins/indent_blackline.lua
+++ .config/sub-nvim/lua/plugins/indent_blackline.lua
@@ -1,15 +1,16 @@
 return {
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
-  config = function()
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    require('indent_blankline').setup {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-      -- ex
-      space_char_blankline = " ",
-      show_current_context = true,
-      show_current_context_start = true,
-    }
-  end
+  main = "ibl",
+  opts = {
+    indent = {
+      char = '┊'
+    },
+    scope = {
+      enabled = true,
+      show_start = true,
+    },
+    whitespace = {
+      remove_blankline_trail = false,
+    },
+  },
 }
