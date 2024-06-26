5e7a0a4a68b8d99cd969899cba051b37aa29cf90
txxxxc <tomoya113.code@gmail.com>
2024-06-26 13:27:57 +0900

    update: config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,22 @@
 # Returns 0 if fish is interactive - that is, connected to a keyboard.
-if status is-interactive
-  ## エラー出るけどasdf自体は動いているので一旦エラーが出力されないように握りつぶします
-  eval (/opt/homebrew/bin/brew shellenv)
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
-end
+fish_add_path "$ASDF_DIR/bin"
+fish_add_path "$HOME/.asdf/shims"
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# if status is-interactive
+#   ## エラー出るけどasdf自体は動いているので一旦エラーが出力されないように握りつぶします
+#   eval (/opt/homebrew/bin/brew shellenv)
+#   source /opt/homebrew/opt/asdf/libexec/asdf.fish &> /dev/null
+# end
+
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx EDITOR nvim
-fish_add_path $HOME/bin
+# fish_add_path $HOME/bin
 fish_add_path /opt/homebrew/bin
 
 # /Users/tomoya_tanaka/.asdf/installs/golang/1.21.0
 # golang
+set -gx ASDF_GOLANG_MOD_VERSION_ENABLED false
 set -x GOPATH (asdf where golang)
 set -gx GOROOT (asdf where golang)/go
 set -x GOBIN (asdf where golang)/packages/bin
@@ -45,7 +51,6 @@ zoxide init fish | source
 abbr -a -- L '--position anywhere --set-cursor ! ! | less' # imported from a universal variable, see `help abbr`
 abbr -a -- atf 'atcoder first' # imported from a universal variable, see `help abbr`
 abbr -a -- dc docker-compose # imported from a universal variable, see `help abbr`
-abbr -a -- gi 'gh issue list' # imported from a universal variable, see `help abbr`
 abbr -a -- sb storybook # imported from a universal variable, see `help abbr`
 abbr -a -- pnpm 'corepack pnpm' # imported from a universal variable, see `help abbr`
 abbr -a -- foo '--set-cursor bar % baz' # imported from a universal variable, see `help abbr`
@@ -71,7 +76,10 @@ abbr -a lg "lazygit" # imported from a universal variable, see `help abbr`
 abbr -a cat "bat --theme=Nord" # imported from a universal variable, see `help abbr`
 abbr -a stime "/usr/bin/time -p" # imported from a universal variable, see `help abbr
 abbr -a gsw "git switch"
+abbr -a lsa "ls -la" # imported from a universal variable, see `help abbr
+abbr -a giv "gh issue list | fzf | awk '{print $1}' | xargs gh issue view --web"
+abbr -a gpv "gh pr list | fzf | awk '{print $1}' | xargs gh pr view --web"
 
 # gh-search-pr-review-requestedのabbr
 abbr -a -- grr 'gh-search-pr-review-requested' # imported from a universal variable, see `help abbr`
-
+set -x PATH (pwd)"/git-fuzzy/bin:$PATH"
